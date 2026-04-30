2e3d3080a3a4e04c59fde6a61559baeb80e2e43f
sushilkumar9096 <107684018+sushilkumar9096@users.noreply.github.com>
2026-04-30 13:30:01 +0530

    Add project files.

---
+++ Student_Management_System/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using global::Student_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+using Student_Management_System.Models;
+
+namespace Student_Management_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext
+            (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Student_Management_System/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Management_System.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Course { get; set; } = string.Empty;
+
+        [Required]
+        public string Mobile { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
